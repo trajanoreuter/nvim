cb44da76c310fd35fffc3bc18167f7dacf36d9d0
Trajano Reuter <trajanoreuter@gmail.com>
2022-02-24 18:14:27 -0300

    add surround plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,7 +59,7 @@ return packer.startup(function(use)
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
   use "christoomey/vim-tmux-navigator"
-
+  use "tpope/vim-surround"
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "morhetz/gruvbox"
