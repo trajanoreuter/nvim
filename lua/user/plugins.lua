9e1501d5655aa0440809e9d9ff1e9ff2174913de
Trajano Reuter <trajanoreuter@gmail.com>
2023-03-08 13:36:32 -0300

    feat: add copilot

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -124,6 +124,7 @@ return packer.startup(function(use)
 
   -- Git
   use "lewis6991/gitsigns.nvim"
+  use "github/copilot.vim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
