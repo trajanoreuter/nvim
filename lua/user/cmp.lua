cc1e534f4d760b896eff70e3ce20e06cb2a122c2
Trajano Reuter <trajanoreuter@gmail.com>
2023-03-08 13:32:30 -0300

    chore: bump versions

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -112,6 +112,7 @@ cmp.setup {
   sources = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
+    { name = "cmp_tabnine" },
     {
       name = 'buffer',
       option = {
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -91,6 +91,11 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
+  use {
+    'tzachar/cmp-tabnine',
+    run='./install.sh',
+    requires = 'hrsh7th/nvim-cmp'
+  }
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
